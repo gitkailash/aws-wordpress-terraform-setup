98db247279476b8706852a4dba76f28b854ad0c1
Kailash Yadav <iamkailashofficial@gmail.com>
2024-09-09 16:20:40 +0545

    validation added

--- variable.tf
+++ variable.tf
@@ -61,3 +61,21 @@ variable "common_ingress_rule" {
       cidr_blocks = ["0.0.0.0/0"]
   }]
 }
+
+variable "resource_tags" {
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default = {
+    project     = "wordpress-deployment"
+    environment = "dev"
+  }
+
+  validation {
+    condition     = length(var.resource_tags["project"]) <= 16 && length(regexall("[^a-zA-Z0-9-]", var.resource_tags["project"])) == 0
+    error_message = "The project tag must be no more than 16 characters, and only contain letters, numbers, and hyphens."
+  }
+  validation {
+    condition     = length(var.resource_tags["environment"]) <= 8 && length(regexall("[^a-zA-Z0-9-]", var.resource_tags["environment"])) == 0
+    error_message = "The environment tag must be no more than 8 characters, and only contain letters, numbers, and hyphens."
+  }
+}
